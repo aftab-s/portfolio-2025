72845f22a7c6c6df2523850174bfcca41ede58a1
Aftab S <aftab.mehrab@gmail.com>
2025-01-07 22:30:48 +0530

    fixed form design

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -10,8 +10,8 @@ const Pricing = () => {
         <div className="text-center mb-12">
           <Heading tag="Contact Me" title="Get in Touch" />
         </div>
-        <div className="max-w-lg mx-auto p-1 bg-gradient-to-r from-yellow-200 via-red-200 via-blue-400 via-teal-400 to-purple-500 rounded-lg">
-          <form className="max-w-lg mx-auto bg-no-repeat p-8 rounded-lg shadow-lg bg-black">
+        <div className="max-w-lg mx-auto p-0.5 bg-gradient-to-r from-yellow-200 via-red-200 via-blue-400 via-teal-400 to-purple-500 rounded-lg">
+        <form className="max-w-lg mx-auto bg-no-repeat p-8 rounded-lg shadow-lg bg-n-7">
             <div className="mb-6">
               <label htmlFor="name" className="block text-gray-200 text-sm font-thin mb-2">
                 Name
@@ -20,43 +20,47 @@ const Pricing = () => {
                 type="text"
                 id="name"
                 name="name"
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                placeholder="Your name"
+                className="w-full px-3 py-2 border border-n-6 rounded-lg text-gray-400 focus:outline-none focus:border-blue-500 bg-n-7 placeholder:text-sm"
                 required
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+              <label htmlFor="email" className="block text-gray-200 text-sm font-thin mb-2">
                 Email
               </label>
               <input
                 type="email"
                 id="email"
                 name="email"
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                placeholder="Your email"
+                className="w-full px-3 py-2 border border-n-6 rounded-lg text-gray-400 focus:outline-none focus:border-blue-500 bg-n-7 placeholder:text-sm"
                 required
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="subject" className="block text-gray-700 text-sm font-bold mb-2">
+              <label htmlFor="subject" className="block text-gray-200 text-sm font-thin mb-2">
                 Subject
               </label>
               <input
                 type="text"
                 id="subject"
                 name="subject"
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                placeholder="The subject line"
+                className="w-full px-3 py-2 border border-n-6 rounded-lg text-gray-400 focus:outline-none focus:border-blue-500 bg-n-7 placeholder:text-sm"
                 required
               />
             </div>
             <div className="mb-6">
-              <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">
+              <label htmlFor="message" className="block text-gray-200 text-sm font-thin mb-2">
                 Message
               </label>
               <textarea
                 id="message"
                 name="message"
                 rows="5"
-                className="w-full px-3 py-2 border border-gray-300 rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
+                placeholder="Type you message here"
+                className="w-full px-3 py-2 border border-n-6 rounded-lg text-gray-400 focus:outline-none focus:border-blue-500 bg-n-7 placeholder:text-sm"
                 required
               ></textarea>
             </div>
